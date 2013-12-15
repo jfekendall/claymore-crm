80be83d0c311d150d8b4d21207935a2419533c4d
jfekendall <jfekendall@gmail.com>
2013-12-15 17:57:27 -0500

    Added DB config options. Included in index for maximum possible happiness.

--- config.php
+++ config.php
@@ -7,4 +7,11 @@ $config['smtp_port'] = '';
 $config['smtp_default_sender'] = '';
 $config['smtp_default_password'] = '';
 
+//Database Config
+$config['db_flavor'] = 'mysql';
+$config['db_hostname'] = '';
+$config['db_username'] = '';
+$config['db_password'] = '';
+
+
 ?>
--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 session_start();
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+//Set Globals from config file
+require_once("config.php");
 require_once("includes/classes/mailer.php");
 echo "Hello World<br>
     <img src='images/icon.png'>";
