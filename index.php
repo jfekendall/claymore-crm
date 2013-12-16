18083ae7407e15a0547934737db9fd2a46b59fc4
jfekendall <jfekendall@gmail.com>
2013-12-15 19:49:27 -0500

    Added database connection based on what flavor is chosen

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
+
 session_start();
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
+//error_reporting(E_ALL);
+//ini_set('display_errors', '1');
 //Set Globals from config file
 require_once("config.php");
+if (!empty($config['db_flavor']) && !empty($config['db_hostname'])) { //Assume the rest is configured and go for it
+    require_once('includes/db/' . $config['db_flavor'] . '.php');
+}
 require_once("includes/classes/mailer.php");
 echo "Hello World<br>
     <img src='images/icon.png'>";
