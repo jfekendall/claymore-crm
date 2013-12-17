2e67bbd178208c1a9e8c82ebb8a60e4a92999829
jfekendall <jfekendall@gmail.com>
2013-12-16 19:40:05 -0500

    Added users table

--- config.php
+++ config.php
@@ -2,7 +2,9 @@
 $GLOBALS = array();
 
 //User Authentication
-$GLOBALS['auth_method'] = 'local'; //local, LDAP
+$GLOBALS['auth_method'] = 'local'; //local, ldap
+$GLOBALS['auth_expire_time'] = '3600';
+$GLOBALS['auth_password_hash_algo'] = 'sha512'; //See http://www.php.net/manual/en/function.hash-algos.php for supported algorythms
 $GLOBALS['ldap_host'] = '';//optional
 $GLOBALS['ldap_port'] = '';//optional
 
@@ -17,9 +19,9 @@ $GLOBALS['db_flavor'] = 'mysqli'; //mysqli or mssql
 $GLOBALS['db_database'] = 'claymore';
 $GLOBALS['db_table_prefix'] = '';
 $GLOBALS['db_domain_name'] = ''; //optional
-$GLOBALS['db_hostname'] = '';
-$GLOBALS['db_username'] = '';
-$GLOBALS['db_password'] = '';
+$GLOBALS['db_hostname'] = 'localhost';
+$GLOBALS['db_username'] = 'root';
+$GLOBALS['db_password'] = 'asdf'; //It's the dev machine. Don't judge me *cries in a corner*
 
 
 ?>
