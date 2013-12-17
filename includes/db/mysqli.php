69bf520c0fd27bd1b71cff0357018ca211c51e9f
jfekendall <jfekendall@gmail.com>
2013-12-16 19:41:02 -0500

    Added support for GLOBALS

--- includes/db/mysqli.php
+++ includes/db/mysqli.php
@@ -1,6 +1,6 @@
 <?php
-$db = new mysqli($config['db_hostname'], $config['db_username'], $config['db_password'], $config['db_database']);
-if($db->connect_errno > 0){
-    die('Something is wrong in your configuration. Server says: [' . $db->connect_error . ']');
+$GLOBALS['db'] = new mysqli($GLOBALS['db_hostname'], $GLOBALS['db_username'], $GLOBALS['db_password'], $GLOBALS['db_database']);
+if($GLOBALS['db']->connect_errno > 0){
+    die('Something is wrong in your configuration. Server says: [' . $GLOBALS['db']->connect_error . ']');
 }
 ?>
