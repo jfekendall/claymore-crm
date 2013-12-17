75af12554ba7bdc2c77edd208236ef2f509929e3
jfekendall <jfekendall@gmail.com>
2013-12-16 19:41:15 -0500

    Initial Commit

--- includes/classes/login.php
+++ includes/classes/login.php
@@ -14,17 +14,37 @@ class login {
 
     private $user;
     private $password;
-    private $auth_method;
 
     public function __construct($user, $password) {
+        $supported_methods = array('local', 'ldap');
         $this->user = $user;
         $this->password = $password;
-        $this->auth_method = $GLOBALS[$config['auth_method']];
-        echo $this->auth_method;
+        if (in_array($GLOBALS['auth_method'], $supported_methods)) {
+            $this->$GLOBALS['auth_method']();
+        } else {
+            die("Your auth_method value is not supported");
+        }
     }
 
-    private function local_login() {
-        
+    private function local() {
+        $allowed = "
+            SELECT id
+            FROM `{$GLOBALS['db_table_prefix']}users`
+            WHERE 
+                `username` = '$this->user'
+            AND
+                `password` = '$this->password'";
+        if (!$result = $GLOBALS['db']->query($allowed)) {
+            die('users table is not configured');
+        } else {
+            if ($id = $result->fetch_assoc()) {
+                setcookie("claymore_user", $id['id'], time() + $GLOBALS['auth_expire_time']);
+                //future home of 'you are allowed in stuff'
+                //echo hash($GLOBALS['auth_password_hash_algo'], $this->password);
+            } else {
+                echo "Username or password is incorrect"; //future home of redirect back to login with error.
+            }
+        }
     }
 
 }
