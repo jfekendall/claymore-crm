8124703d0aef49743b2aaeb4bdfebcb24726098a
jfekendall <jfekendall@gmail.com>
2013-12-16 19:40:41 -0500

    Testing for local login

--- index.php
+++ index.php
@@ -2,20 +2,23 @@
 
 session_start();
 //UNCOMMENT FOR DEBUG
-//error_reporting(E_ALL);
-//ini_set('display_errors', '1');
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
 //Set Globals from config file
 require_once("config.php");
 //Connect to a database if it is configured
-if (!empty($config['db_flavor']) && !empty($config['db_hostname'])) { //Assume the rest is configured and go for it
-    require_once("includes/db/{$config['db_flavor']}.php");
+if (!empty($GLOBALS['db_flavor']) && !empty($GLOBALS['db_hostname'])) { //Assume the rest is GLOBALSured and go for it
+    require_once("includes/db/{$GLOBALS['db_flavor']}.php");
 } else {
     echo "Configure a database to continue"; //Coming soon! Initialization of the setup script!
 }
+require_once("includes/classes/login.php");
+new login('justin', 'wieners');
 //Get the mailer class set-up
 require_once("includes/classes/mailer.php");
 
 //Some placeholder stuff to make sure nothing died in initialization
-echo "Hello World<br>
+echo "<br>
     <img src='images/icon.png'>";
+
 ?>
