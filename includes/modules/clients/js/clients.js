c40909fa36dc11d1925169f1753daf0a2c8454bf
jfekendall <jfekendall@gmail.com>
2013-12-28 18:55:51 -0500

    Can now add B2B Clients

--- includes/modules/clients/js/clients.js
+++ includes/modules/clients/js/clients.js
@@ -13,6 +13,57 @@ $(function() {
             $('select[name=state] option[value=' + correctArray[1] + ']').attr('selected', true);
         }
     });
+    $('.addNewClient').click(function() {
+        var required = new Array('business_name', 'password', 'email', 'phone', 'street_1', 'city', 'postal_code');
+        var stop = 0;
+        var ohCrap = '';
+        var ajaxUrl = this_location + "/includes/modules/clients/js/clients.php?i=newClient";
+        required.forEach(function(req) {
+            if (!$('input[name=' + req + ']').val()) {
+                req = req.replace('_', ' ');
+                req = req.replace(/\w\S*/g, function(txt) {
+                    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+                });
+                ohCrap = ohCrap + "Required field " + req + " is empty.<br>";
+                stop = 1;
+            } else {
+                if (req === 'phone') {
+                    var phone = phoneFormat($('input[name=' + req + ']').val());
+                    ajaxUrl = ajaxUrl + "&" + req + "=" + escape(phone);
+                } else {
+                    ajaxUrl = ajaxUrl + "&" + req + "=" + escape($('input[name=' + req + ']').val());
+                }
+            }
+        });
+        if (stop === 1) {
+            $('.alert-danger').slideToggle('fast');
+            $('.alert').addClass('alert-danger');
+            $('.alert').html(ohCrap);
+            $('.alert-danger').slideToggle('fast');
+            return;
+        }
+        ajaxUrl = ajaxUrl + "&is_main_office=1";
+        ajaxUrl = ajaxUrl + "&street_2=" + escape($('input[name=street_2]').val());
+        ajaxUrl = ajaxUrl + "&state=" + $('select[name=state]').val();
+
+        var error = $.ajax({
+            url: ajaxUrl,
+            async: false
+        }).responseText;
+        if (error) {
+            $('.alert-success').slideToggle('fast');
+            $('.alert').addClass('alert-success');
+            $('.alert').removeClass('alert-danger');
+            $('.alert').html(error);
+            $('.alert-success').slideToggle('fast');
+        }
+    });
 }
 );
+
+function phoneFormat(str) {
+
+    return str.replace(/(\d{3})(\d{3})(\d{4})/, '$1-$2-$3');
+
+}
 
